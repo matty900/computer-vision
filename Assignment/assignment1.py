1a36ef2b709917abe0df8cf0dbabcfff52036747
Mahdi Zangeneh <mzangeneh2@myseneca.ca>
2025-02-09 17:21:41 -0500

    "chnage thicknes for assignment 1"

--- Assignment/assignment1.py
+++ Assignment/assignment1.py
@@ -41,7 +41,7 @@ img = "Assignment/Dog and Cat.jpg"
 image = cv2.imread(img)
  
 cv2.rectangle(image,(384,0),(510,128),(0,255,0),-1)
-cv2.putText(image, "text", (420,80) , cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), thickness=2)
+cv2.putText(image, "text", (420,80) , cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), thickness=4)
 cv2.imshow("image",image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
